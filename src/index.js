f7e2f6342e1ab71e80c85d8f9cb6b70854fb6e5a
Adan Rodriguez <adan.rodriguez.fusta@gmail.com>
2023-02-27 19:30:45 -0300

    🔨 Refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 // import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/style.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+createRoot(document.getElementById("root")).render(
   // La utilización de Helmet tiene sentido si la página se renderiza del lado del servidor.
   // TODO: renderizar del lado del servidor
   // <HelmetProvider>
